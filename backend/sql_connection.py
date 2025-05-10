2d62129b42f888ec23a0ef39c93ea857b5c2e8a4
Sarthaksource <78723977+Sarthaksource@users.noreply.github.com>
2025-05-10 17:09:08 +0530

    Update sql_connection.py

--- backend/sql_connection.py
+++ backend/sql_connection.py
@@ -2,6 +2,11 @@ import os
 import mysql.connector
 
 def get_sql_connection():
+    print("MYSQLHOST:", os.environ.get("MYSQLHOST"))
+    print("MYSQLUSER:", os.environ.get("MYSQLUSER"))
+    print("MYSQLPASSWORD:", os.environ.get("MYSQLPASSWORD"))
+    print("MYSQLDATABASE:", os.environ.get("MYSQLDATABASE"))
+    print("MYSQLPORT:", os.environ.get("MYSQLPORT"))
     return mysql.connector.connect(
         host=os.environ.get("MYSQLHOST"),
         user=os.environ.get("MYSQLUSER"),
