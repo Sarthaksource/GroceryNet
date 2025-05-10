039cd0002c8b9f9ac169004b6d61c198fff46ba6
Sarthaksource <78723977+Sarthaksource@users.noreply.github.com>
2025-05-10 14:32:36 +0530

    Update sql_connection.py

--- backend/sql_connection.py
+++ backend/sql_connection.py
@@ -1,16 +1,11 @@
-import mysql.connector
 import os
-
-__cnx = None
+import mysql.connector
 
 def get_sql_connection():
-    global __cnx
-    if __cnx is None:
-        __cnx = mysql.connector.connect(
-            host=os.environ.get("MYSQLHOST"),
-            user=os.environ.get("MYSQLUSER"),
-            password=os.environ.get("MYSQLPASSWORD"),
-            database=os.environ.get("MYSQLDATABASE"),
-            port=int(os.environ.get("MYSQLPORT", 3306))
-        )
-    return __cnx
+    return mysql.connector.connect(
+        host=os.environ.get("MYSQLHOST"),
+        user=os.environ.get("MYSQLUSER"),
+        password=os.environ.get("MYSQLPASSWORD"),
+        database=os.environ.get("MYSQLDATABASE"),
+        port=int(os.environ.get("MYSQLPORT", 3306))
+    )
